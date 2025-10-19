4507806748b3c10d18a0dde8f1701ec3c24f814d
kobebenavente <kobeb@uw.edu>
2025-10-19 12:12:46 -0700

    merged and made some minor changes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,27 @@
-// app.js
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from '../openapi.json' with { type: 'json' };
 import showsRouter from './routes/shows.js';
-// import { checkApiKey } from './middleware/authMiddleware.js'; // ← comment out
+import { checkApiKey } from './middleware/authMiddleware.js'; 
 
 dotenv.config();
+
+console.log('Loaded API key:', process.env.API_KEY);
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// app.use(checkApiKey); // ← comment out
+ app.use(checkApiKey); 
 
 app.get('/', (req, res) => res.json({ message: 'Welcome to Group 5 TV API!' }));
 app.use('/api/v1/shows', showsRouter);
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
